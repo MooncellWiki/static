6c31988d9f1be3856148926668e1aa502126ef4e
StarHeartHunt <starheart233@gmail.com>
2025-10-18 11:57:31 +0800

    feat: add horain to special sponsor

--- sponsorkit.config.ts
+++ sponsorkit.config.ts
@@ -1,19 +1,31 @@
 import { defineConfig, tierPresets, type BadgePreset } from "@mooncellwiki/sponsorkit";
 import fs from "fs";
 
-function toBase64(filePath) {
+function toBase64(filePath: string) {
   const img = fs.readFileSync(filePath);
 
   return Buffer.from(img).toString("base64");
 }
 
 const SKLAND_LOGO = (width: number, y: number) => `
-<a xlink:href="https://www.skland.com" class="sponsorkit-link" target="_blank" id="SKLand">
+<a xlink:href="https://www.skland.com/" class="sponsorkit-link" target="_blank" id="SKLand">
 <svg x="${
-  (width - 361) / 2
+  width / 2 - 361
 }" y="${y}" width="361" height="86" viewBox="0 0 361 86" fill="none" xmlns="http://www.w3.org/2000/svg">
 <image x="0" y="0" width="361" height="86" xlink:href="${
   "data:image/png;base64," + toBase64("assets/skland.png")
+}"/>
+</svg>
+</a>
+`;
+
+const HORAIN_LOGO = (width: number, y: number) => `
+<a xlink:href="https://www.horain.net/" class="sponsorkit-link" target="_blank" id="Horain">
+<svg x="${
+  width / 2 - 43
+}" y="${y}" width="361" height="86" viewBox="0 0 361 86" fill="none" xmlns="http://www.w3.org/2000/svg">
+<image x="0" y="0" width="361" height="86" xlink:href="${
+  "data:image/png;base64," + toBase64("assets/horain/340x100.png")
 }"/>
 </svg>
 </a>
@@ -87,7 +99,7 @@ export default defineConfig({
             .addSpan(20)
             .addText("特别赞助", "sponsorkit-tier-title")
             .addSpan(10)
-            .addRaw(SKLAND_LOGO(config.width!, compose.height))
+            .addRaw(SKLAND_LOGO(config.width!, compose.height)+HORAIN_LOGO(config.width!+100, compose.height))
             .addSpan(130);
         }
       },
